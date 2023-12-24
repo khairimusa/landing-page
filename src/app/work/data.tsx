406fdbcf586be8a46690492af925df66e7cafff3
khairimusa <khairimusa60@gmail.com>
2023-12-24 18:57:34 +0800

    update description

--- src/app/work/data.tsx
+++ src/app/work/data.tsx
@@ -3,8 +3,8 @@ export const mails = [
     id: '1',
     name: 'Software Engineer',
     subject: 'Airasia Berhad',
-    link: 'https://google.com',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    link: 'https://www.capitala.com',
+    text: `Lead the blog initiatives. Increased users' time retention by 8%. Integrated webhooks from client's ERP system to automate email sending on each status changes.`,
     date: 'Dec 2022 - Present',
     products: [
       {
@@ -38,8 +38,10 @@ export const mails = [
     id: '2',
     name: 'Software Developer',
     subject: 'Dex Labs Sdn Bhd',
-    link: 'https://google.com',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    link: 'https://www.dex-labs.com/',
+    text: `
+    Leveraging company's in house React component library to build client's web and mobile apps. Integrated SNYK Open Source into a payment gateway project's deployment pipeline for a vulnerability scanning to make sure it met the PCI DSS certification. Developed, Test & Managed the release of a Unity AR mobile application on Apple App Store, Google Play Store & Huawei App Gallery. Make sure that the application is free of bugs and performant across low to high end mobile devices. Ohsem! AR now has over 500+ downloads and a 4.5+ star rating across 3 app stores.
+    `,
     date: 'Feb - Dec 2022',
     products: [
       {
@@ -77,8 +79,8 @@ export const mails = [
     id: '3',
     name: 'Web Application Developer',
     subject: 'Appasia Berhad',
-    link: 'https://google.com',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    link: 'https://www.appasia.com/',
+    text: `Modernized an existing yellopages.my's admin dashboard used by Advertisers. Built UI from Adobe XD mockup to real working screen. Achieving responsive design Achieving responsive design by using css media query. Developed modularized Angular components, to increase in app reusability. Took advantage of SCSS to make styling of the application less repeating. Utilizing PHP Laravel to ease back-end development and migration of databases.`,
     date: 'July - Feb 2022',
     products: [
       {
@@ -92,9 +94,8 @@ export const mails = [
     id: '4',
     name: 'Junior Software Engineer',
     subject: 'Kerry Logistics',
-    link: 'https://google.com',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    date: '2020 - 2021',
+    link: 'https://www.kerrylogistics.com/',
+    text: `Involved in Kerry Warehouse Management V3 project for Kerry Logistics. A global focus product that aims to replace the current V2 for all their warehouses around the globe using AWS cloud tech.`,
     products: [
       {
         title: 'Kerry WMS v3',
@@ -107,8 +108,8 @@ export const mails = [
     id: '5',
     name: 'Software Engineer Intern',
     subject: 'Theta Service Partner',
-    link: 'https://google.com',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    link: 'https://www.thetasp.com/',
+    text: `Involved in corporate loan origination system project for international bank brands. Worked under a project with a bank based in Taiwan. Developed Java API using Spring MVC and tested all the APIs using Swagger UI client. Also worked with the base team developing reusable angular components`,
     date: 'Jan - May 2020',
     products: [
       {
