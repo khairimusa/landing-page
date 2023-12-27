9037296d5ea2a7c44c8c6ffeb30ca6ee66e56296
khairimusa <khairimusa60@gmail.com>
2023-12-27 23:16:46 +0800

    responsive pdf viewer

--- src/custom/PdfViewer.tsx
+++ src/custom/PdfViewer.tsx
@@ -14,13 +14,15 @@ export default function PdfViewer() {
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="bg-gray-100 flex flex-col rounded-t-[10px] h-full mt-24 max-h-[96%] fixed bottom-0 left-0 right-0">
-          <div className="p-4 bg-white dark:bg-slate-900 rounded-t-[10px] flex-1">
+        <Drawer.Content className="bg-gray-100 flex flex-col rounded-t-[10px] h-full mt-24 fixed max-h-[85%] bottom-0 left-0 right-0">
+          <div className="bg-white dark:bg-slate-900 rounded-t-[10px] py-4">
             <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-gray-300 mb-8" />
-            <iframe
-              src="https://firebasestorage.googleapis.com/v0/b/kai-portfolio-7c5c4.appspot.com/o/Mohammad%20Khairi%20Bin%20Musa%20CV%202023.pdf?alt=media&token=afe82669-e70a-4f1a-a539-1bad0b22792c"
-              className="w-1/2 mx-auto h-full"
-            ></iframe>
+            <div className="absolute w-full h-full overflow-hidden">
+              <iframe
+                src="https://firebasestorage.googleapis.com/v0/b/kai-portfolio-7c5c4.appspot.com/o/Khairi-Resume-2024.pdf?alt=media&token=b3b15ecf-47cc-48d7-96bd-9dbbc9b18c41"
+                className="relative top-0 left-0 bottom-0 right-0 w-full h-full border-none overflow-hidden"
+              ></iframe>
+            </div>
           </div>
         </Drawer.Content>
       </Drawer.Portal>
