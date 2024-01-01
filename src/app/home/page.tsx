49dbac1c5a3474c791d545fa3b56046a39cedf3e
khairimusa <khairimusa60@gmail.com>
2024-01-02 01:25:36 +0800

    remove unused

--- src/app/home/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <section className="flex justify-between flex-wrap border-b px-4 pb-4">
-      <h1 className="text-6xl font-bold tracking-tight">Section 1</h1>
-    </section>
-  );
-}
